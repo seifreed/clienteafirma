b0e2fb946fa1dc4099cad99f63aba0554ad2c394
Marc R. <mriverolopez@gmail.com>
2026-07-17 00:29:36 +0200

    fix: reject incomplete jarm array paths

--- afirma-eudiw-bridge/src/test/java/es/gob/afirma/eudiw/TestAuthorizationRequest.java
+++ afirma-eudiw-bridge/src/test/java/es/gob/afirma/eudiw/TestAuthorizationRequest.java
@@ -439,7 +439,7 @@ final class TestAuthorizationRequest {
 		assertThrows(JOSEException.class, () -> JarmAuthorizationResponse.verify(
 				malformedDescriptorPathSubmissionJwt.serialize(), verifier,
 				"https://verifier.example.es", "state-1")); //$NON-NLS-1$ //$NON-NLS-2$
-		for (final String malformedPath : List.of("$.", "$[")) { //$NON-NLS-1$ //$NON-NLS-2$
+		for (final String malformedPath : List.of("$.", "$[", "$[]", "$[0")) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 			final SignedJWT barePrefixPathSubmissionJwt = new SignedJWT(
 					jarmHeader(),
 					new JWTClaimsSet.Builder(jwt.getJWTClaimsSet())
